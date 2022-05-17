efd4bb32a480b89995839c71e3d9cad94b7d36a9
sebastianbocul <seba23495@gmail.com>
2022-05-17 18:14:47 +0200

    Created InsertNewNote use case

--- app/src/main/java/com/sebix/cleanarchitecture/business/interactors/notelist/InsertNewNote.kt
+++ app/src/main/java/com/sebix/cleanarchitecture/business/interactors/notelist/InsertNewNote.kt
@@ -1,4 +1,68 @@
 package com.sebix.cleanarchitecture.business.interactors.notelist
 
-class InsertNewNote {
+import com.sebix.cleanarchitecture.business.data.cache.abstraction.NoteCacheDataSource
+import com.sebix.cleanarchitecture.business.data.network.abstraction.NoteNetworkDataSource
+import com.sebix.cleanarchitecture.business.domain.model.Note
+import com.sebix.cleanarchitecture.business.domain.model.NoteFactory
+import com.sebix.cleanarchitecture.business.domain.state.*
+import com.sebix.cleanarchitecture.framework.presentation.notelist.state.NoteListViewState
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import java.util.*
+
+class InsertNewNote(
+    private val noteCacheDataSource: NoteCacheDataSource,
+    private val noteNetworkDataSource: NoteNetworkDataSource,
+    private val noteFactory: NoteFactory
+) {
+    fun insertNewNote(
+        id: String? = null,
+        title: String,
+        stateEvent: StateEvent
+    ): Flow<DataState<NoteListViewState>> = flow {
+        val newNote = noteFactory.createSingleNote(
+            id = id ?: UUID.randomUUID().toString(),
+            title = title,
+            body = ""
+        )
+        val cacheResult = noteCacheDataSource.insertNote(newNote)
+        var cacheResponse: DataState<NoteListViewState>? = null
+        if (cacheResult > 0) {
+            val viewState = NoteListViewState(
+                newNote = newNote
+            )
+            cacheResponse = DataState.data(
+                response = Response(
+                    message = INSERT_NOTE_SUCCESS,
+                    uiComponentType = UIComponentType.Toast(),
+                    messageType = MessageType.Success()
+                ),
+                data = viewState,
+                stateEvent = stateEvent
+            )
+        } else {
+            cacheResponse = DataState.data(
+                response = Response(
+                    message = INSERT_NOTE_FAILED,
+                    uiComponentType = UIComponentType.Toast(),
+                    messageType = MessageType.Error()
+                ),
+                data = null,
+                stateEvent = stateEvent
+            )
+        }
+        emit(cacheResponse)
+        updateNetwork(cacheResponse.stateMessage?.response?.message, newNote)
+    }
+
+    private suspend fun updateNetwork(cacheResponse: String?, newNote: Note) {
+        if (cacheResponse.equals(INSERT_NOTE_SUCCESS)) {
+            noteNetworkDataSource.insertOrUpdateNote(newNote)
+        }
+    }
+
+    companion object {
+        const val INSERT_NOTE_SUCCESS = "Successfully inserted new note"
+        const val INSERT_NOTE_FAILED = "Failed to insert new note"
+    }
 }
