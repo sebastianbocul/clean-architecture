2c4ddbb20c0fa7c26a36cc4be922e87ca54a1a97
sebastianbocul <seba23495@gmail.com>
2022-05-19 21:49:26 +0200

    Created SearchNotes use case

--- app/src/main/java/com/sebix/cleanarchitecture/business/interactors/notelist/SearchNotes.kt
+++ app/src/main/java/com/sebix/cleanarchitecture/business/interactors/notelist/SearchNotes.kt
@@ -1,4 +1,68 @@
 package com.sebix.cleanarchitecture.business.interactors.notelist
 
-class SearchNotes {
+import com.sebix.cleanarchitecture.business.data.cache.CacheResponseHandler
+import com.sebix.cleanarchitecture.business.data.cache.abstraction.NoteCacheDataSource
+import com.sebix.cleanarchitecture.business.data.util.safeCacheCall
+import com.sebix.cleanarchitecture.business.domain.model.Note
+import com.sebix.cleanarchitecture.business.domain.state.*
+import com.sebix.cleanarchitecture.framework.presentation.notelist.state.NoteListViewState
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class SearchNotes(
+    private val noteCacheDataSource: NoteCacheDataSource
+) {
+    fun searchNotes(
+        query: String,
+        filerAndOrder: String,
+        page: Int,
+        stateEvent: StateEvent
+    ): Flow<DataState<NoteListViewState>> = flow {
+        var updatePage = page
+        if (page <= 0) {
+            updatePage = 1
+        }
+        val cacheResult = safeCacheCall(IO) {
+            noteCacheDataSource.searchNotes(
+                query = query,
+                filterAndOrder = filerAndOrder,
+                page = updatePage
+            )
+        }
+
+        val response = object : CacheResponseHandler<NoteListViewState, List<Note>>(
+            response = cacheResult,
+            stateEvent = stateEvent
+        ) {
+            override fun handleSuccess(resultObj: List<Note>): DataState<NoteListViewState> {
+                var message: String? = SEARCH_NOTES_SUCCESS
+                var uiComponentType: UIComponentType = UIComponentType.None()
+                if (resultObj.isEmpty()) {
+                    message = SEARCH_NOTES_NO_MATCHING_RESULTS
+                    uiComponentType = UIComponentType.Toast()
+                }
+                return DataState.data(
+                    response = Response(
+                        message = message,
+                        uiComponentType = uiComponentType,
+                        messageType = MessageType.Success()
+                    ),
+                    data = NoteListViewState(
+                        noteList = ArrayList(resultObj)
+                    ),
+                    stateEvent = stateEvent
+                )
+            }
+        }.getResult()
+        emit(response)
+    }
+
+    companion object {
+        val SEARCH_NOTES_SUCCESS = "Successfully retrieved list of notes."
+        val SEARCH_NOTES_NO_MATCHING_RESULTS = "There are no notes that match that query."
+        val SEARCH_NOTES_FAILED = "Failed to retrieve the list of notes."
+    }
 }
+
+
