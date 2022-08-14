cad02000a880f760a8326721cdf3c3c1fab2e0c4
sebastianbocul <seba23495@gmail.com>
2022-08-14 09:32:01 +0200

    GetNumNotes use case

--- app/src/main/java/com/sebix/cleanarchitecture/business/interactors/notelist/GetNumNotes.kt
+++ app/src/main/java/com/sebix/cleanarchitecture/business/interactors/notelist/GetNumNotes.kt
@@ -1,4 +1,51 @@
 package com.sebix.cleanarchitecture.business.interactors.notelist
 
-class GetNumNotes {
+import com.sebix.cleanarchitecture.business.data.cache.CacheResponseHandler
+import com.sebix.cleanarchitecture.business.data.cache.abstraction.NoteCacheDataSource
+import com.sebix.cleanarchitecture.business.data.util.safeCacheCall
+import com.sebix.cleanarchitecture.business.domain.state.*
+import com.sebix.cleanarchitecture.framework.presentation.notelist.state.NoteListViewState
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class GetNumNotes(
+    private val noteCacheDataSource: NoteCacheDataSource
+) {
+    fun getNumNotes(
+        stateEvent: StateEvent
+    ): Flow<DataState<NoteListViewState>> = flow {
+
+        val cacheResult = safeCacheCall(IO) {
+            noteCacheDataSource.getNumNotes()
+        }
+
+        val response = object : CacheResponseHandler<NoteListViewState, Int>(
+            response = cacheResult,
+            stateEvent = stateEvent
+        ) {
+            override fun handleSuccess(resultObj: Int): DataState<NoteListViewState> {
+
+
+                val viewState = NoteListViewState(
+                    numNotesInCache = resultObj
+                )
+                return DataState.data(
+                    response = Response(
+                        message = GET_NUM_NOTES_SUCCESS,
+                        uiComponentType = UIComponentType.None(),
+                        messageType = MessageType.Success()
+                    ),
+                    data = viewState,
+                    stateEvent = stateEvent
+                )
+            }
+        }.getResult()
+
+    }
+
+    companion object {
+        val GET_NUM_NOTES_SUCCESS = "Successfully retrieved the number of notes from the cache."
+        val GET_NUM_NOTES_FAILED = "Failed to get the number of notes from the cache."
+    }
 }
